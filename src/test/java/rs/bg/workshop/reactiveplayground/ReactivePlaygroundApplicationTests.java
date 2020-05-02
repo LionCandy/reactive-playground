1c138c59d100b3ef61d5dc9dd1869b277c3bc737
Emanuel BG <barreragz.emanuel@gmail.com>
2020-05-01 20:22:25 -0500

    init 0

---
+++ src/test/java/rs/bg/workshop/reactiveplayground/ReactivePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.bg.workshop.reactiveplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
